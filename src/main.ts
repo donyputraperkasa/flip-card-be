dd9ad94c09f3dbfe26584c36370bab5a84c1f3de
albertus magnus dony putra perkasa <donyputraperkasa@gmail.com>
2025-11-26 13:54:37 +0700

    adjust maints

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
-  console.log(`🚀 Server running at: ${await app.getUrl()}`);
+  console.log(`🚀 Server running at : ${await app.getUrl()}`);
 }
 
 bootstrap();
